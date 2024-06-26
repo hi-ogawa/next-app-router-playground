5626f09723979793794ddecdb5e03221bacf87db
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-27 08:43:19 +0900

    chore: more workaround

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,29 @@
 import next from "next/vite";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 
 export default defineConfig({
-  plugins: [next()],
+  plugins: [
+    next(),
+    fixStyledJsx(),
+  ],
 });
+
+/** @todo */
+function fixStyledJsx(): Plugin {
+  return {
+    name: fixStyledJsx.name,
+    resolveId(source, _importer, _options) {
+      if (source === "styled-jsx") {
+        return "\0virtual:styled-jsx"
+      }
+    },
+    load(id, _options) {
+      if (id === "\0virtual:styled-jsx") {
+        return `
+          export const StyleRegistry = null;
+          export const createStyleRegistry = null;
+        `;
+      }
+    },
+  }
+}
