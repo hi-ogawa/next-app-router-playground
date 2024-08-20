70efd41af9ece8d09810bd31ba01684563f843b9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-20 19:09:57 +0900

    chore: test metadata.opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,14 @@ export const metadata: Metadata = {
     default: 'Next.js App Router',
     template: '%s | Next.js App Router',
   },
-  metadataBase: new URL('https://app-router.vercel.app'),
+  metadataBase: new URL('https://app-router-vite.vercel.app'),
   description:
     'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
   openGraph: {
     title: 'Next.js App Router Playground',
     description:
       'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
-    images: [`/api/og?title=Next.js App Router`],
+    images: [`/api/og?title=Next.js App Router on Vite`],
   },
   twitter: {
     card: 'summary_large_image',
